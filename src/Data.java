8cfad0d80232b1638161419391a33acd151f944b
MarcosAur <marcosaureliomendesoliveira@gmail.com>
2021-03-19 18:27:13 -0300

    Adição da tela para receber os dados

--- src/Data.java
+++ src/Data.java
@@ -1,32 +1,47 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-/**
- *
- * @author default
- */
 public class Data {
-    private int dia;
-    private int mes;
-    private int ano;
 
-    public Data(int dia, int mes, int ano) {
-        this.dia = dia;
-        this.mes = mes;
-        this.ano = ano;
-    }
-    
-    public int ValidarIdade(Data d){
-        int idade = d.ano - this.ano;
-        if (d.mes < this.mes){
-            idade -=1 ;
-        }else if ((d.mes == this.mes )&&(d.dia > this.dia)){
+    public static String ValidarIdade(int dia,int mes,int ano){
+        //Objeto
+        SimpleDateFormat formatar = new SimpleDateFormat("dd/MM/yyyy");
+        Date data = new Date();
+
+        //Variaveis
+        int atual[] = new int[3];
+        int valor;
+        int contador = 0;
+        int idade;
+        int modelo = 18;
+        String data_formatada;
+        String[] data_dividida;
+        
+        //Inicio
+        data_formatada = formatar.format(data);
+        data_dividida = data_formatada.split("/");
+        
+        //Construção do vetor Data Atual(separada)
+        while(contador < 3){
+            valor = Integer.parseInt(data_dividida[contador]);
+            atual[contador] = valor;
+            contador++;
+        }
+        
+        idade = atual[2] - ano;
+        
+        if (atual[1] < mes){
+            idade -= 1;
+        }else if ((atual[1] == mes )&&(atual[0] < dia)){
             idade -= 1;
         }
-        return idade;
+        
+        //Verificação
+        if (idade < modelo){
+            return "Menor";
+        }else{
+            return "Maior";
+        }
     }
 
     
